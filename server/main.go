8652d2d9d81df665a044bfdaf26d1d3994a34c89
TheGrids <zolotar333@gmail.com>
2022-03-17 11:39:37 +0300

    adding REST requests to example

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-server/controllers"
 	"go-server/models"
 )
 
@@ -14,6 +15,12 @@ func main() {
 
 	// Database connection
 	models.ConnectionDataBase()
+
+	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
+	r.POST("/books", controllers.CreateBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
 
 	r.Run()
 }
