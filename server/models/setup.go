fb8b661e3a6b4598f52dcc6928bd4239f1e52a59
TheGrids <zolotar333@gmail.com>
2022-03-18 10:28:38 +0300

    edit .env & add .env in docker-compose

--- server/models/setup.go
+++ server/models/setup.go
@@ -6,25 +6,20 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func ConnectionDataBase() {
 	// Checking .env file
-	if err := godotenv.Load(); err != nil {
+	dsn, exist := os.LookupEnv("POSTGRES_CONNECT")
+
+	if !exist {
 		log.Printf("Error loading .env file")
 	}
 
-	hostname := os.Getenv("POSTGRES_HOSTNAME")
-	user := os.Getenv("POSTGRES_USER")
-	password := os.Getenv("POSTGRES_PASSWORD")
-	dbname := os.Getenv("POSTGRES_DB")
-
 	// Database connection
 	// TODO сделать красиво и удобно в будущем
-	dsn := "host=" + hostname + " user=" + user + " password=" + password + " dbname=" + dbname + " port=5432 sslmode=disable"
 	database, err := gorm.Open("postgres", dsn)
 
 	if err != nil {
