8ce1b045a2507129f0d633a2000deec30b8bfdba
TheGrids <zolotar333@gmail.com>
2022-03-20 09:08:54 +0300

    adding gin cors

--- server/main.go
+++ server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go-server/controllers"
 	"go-server/models"
 	"go-server/services"
+	"time"
 )
 
 func main() {
@@ -16,6 +18,14 @@ func main() {
 
 	// Database connection
 	models.ConnectionDataBase()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://xn--e1ameg9a5c.xn--p1ai", "http://xn--80aaca8d.xn--e1ameg9a5c.xn--p1ai"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
+		MaxAge:           1 * time.Minute,
+	}))
 
 	r.GET("/books", controllers.FindBooks)
 	r.GET("/books/:id", controllers.FindBook)
